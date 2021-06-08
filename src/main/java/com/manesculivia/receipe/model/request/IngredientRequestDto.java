be15bd18df6d0c2c3d5d5dec3fc4245a4b7b8b6c
livia_manescu@yahoo.com <livia_manescu@yahoo.com>
2021-06-08 15:47:41 +0300

    add api tests for read and create pantry ingredients

--- src/main/java/com/manesculivia/receipe/model/request/IngredientRequestDto.java
+++ src/main/java/com/manesculivia/receipe/model/request/IngredientRequestDto.java
@@ -1,12 +1,16 @@
 package com.manesculivia.receipe.model.request;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
+@Setter
+@Builder
 @ToString
 public class IngredientRequestDto {
 
